edd8f6e88a3f5e3e5a0050e694851a3a13dc52ba
Kevin Campos <89799176+kvncampos@users.noreply.github.com>
2025-12-24 08:34:29 -0600

    cleaning up constants.py

--- ai_ops/helpers/common/constants.py
+++ ai_ops/helpers/common/constants.py
@@ -4,7 +4,6 @@
 class Urls:
     """Urls."""
 
-    APIGEE_URL = "Apigee URL"
     BASE_URL: str = "https://nautobot-lab.example.com/api"
     NONPROD_URL: str = "https://nautobot-nonprod.example.com"
     PROD_URL: str = "https://nautobot.example.com"
